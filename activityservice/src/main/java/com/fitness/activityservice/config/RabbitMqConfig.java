03ebba0bf240ea7032b84f5092d153ecd7348b76
hk20-0305 <hk20032005@gmail.com>
2026-08-21 22:34:52 +0530

    rabbite

--- activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -12,14 +12,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
-
-    @Value("${rabbitmq.queue.name}")
+    @Value("${rabbitmq.queue.name:activity.queue}")
     private String queue;
 
-    @Value("${rabbitmq.exchange.name}")
+    @Value("${rabbitmq.exchange.name:fitness.exchange}")
     private String exchange;
 
-    @Value("${rabbitmq.routing.key}")
+    @Value("${rabbitmq.routing.key:activity.tracking}")
     private String routingKey;
 
     @Bean
